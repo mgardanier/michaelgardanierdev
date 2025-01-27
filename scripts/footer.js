8674d116c3283cb3f1237895ed4bdc900e318a5d
mgardanier <mgardanier@gmail.com>
2025-01-27 15:42:08 -0700

    Update footer mobile behavior

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,6 +2,11 @@
 fetch('/fragments/footer.html')
     .then(response => response.text())
     .then(data => {
-        document.querySelector('body').insertAdjacentHTML('beforeend', data);
+        const footerDiv = document.querySelector('body');
+        footerDiv.insertAdjacentHTML('beforeend', data);
+        const footer = document.querySelector('footer');
+        if (window.innerWidth < 768) {
+            footer.style.position = 'sticky';
+        }
     })
     .catch(err => console.log('Failed to load footer: ', err));
